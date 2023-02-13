632477d4f3985be3ef345cde1dee218489e2df57
Emma Batson <emmakbat@gmail.com>
2023-02-13 17:30:02 -0500

    added remainder of ITO data

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors
+import matplotlib as mpl
 import numpy as np
 import json
 from enum import Enum, auto
+import tikzplotlib
 
 from VAMASparse import VAMASparser
 from VAMASspecs import *
@@ -10,10 +13,29 @@ from vamas_helpers import *
 class PlotType(Enum):
     spectra = auto()
     high_res = auto()
+    depth = auto()
 
 def main():
     config_folder = 'configs/'
-    config_file = '211124_highres.json'
+    config_file = 'ITOSAcontrol_depth.json'
+    # this variable inserts a vertical offset between spectra for readability
+    # recommended to adjust until it looks right
+    offset = 5000
+
+    id_to_peak = {'In':'Indium 3d5', 'Sn': 'Tin 3d', 'O': 'Oxygen 1s', 'C': 'Carbon 1s'}
+
+    SMALL_SIZE = 12
+    MEDIUM_SIZE = 14
+    BIGGER_SIZE = 16
+
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 
     with open(config_folder+config_file) as json_file:
         config = json.load(json_file)
@@ -23,18 +45,17 @@ def main():
         colors = config['colors']
         plotType = PlotType[config['plot type']]
 
+    parsers = []
+    for filename in filenames:
+        parser = VAMASparser(filepath+filename)
+        experiment, blocks = parser.read_VAMAS()
+
+        parsers.append(parser)
+
     if plotType == PlotType.spectra:
-        offset = 2000
-        plot_spectra(filepath, filenames, labels, colors, offset)
+        x, y = plot_spectra(parsers, labels, colors, offset)
 
     elif plotType == PlotType.high_res:
-        parsers = []
-        for filename, label in zip(filenames, labels):
-            parser = VAMASparser(filepath+filename)
-
-            experiment, blocks = parser.read_VAMAS()
-            parsers.append(parser)
-
         for i in range(len(blocks)):
             for j, parser in enumerate(parsers):
                 x, y, xunits, xlabel, yunits, ylabel = get_binding_vs_y(parser)
@@ -44,6 +65,50 @@ def main():
             plot_formatting(x, xunits, yunits, ylabel)
             plt.title(block_identifier)
             plt.show()
+    elif plotType == PlotType.depth:
+        unique_identifiers = {}
+        for parser in parsers:
+            for i in range(len(blocks)):
+                for j, parser in enumerate(parsers):
+                    x,  y, xunits, xlabel, yunits, ylabel = get_binding_vs_y(parser, i)
+                    block_identifier = parser.get_block_data(VAMASBlockHeader.block_identifier, i)
+
+                    if block_identifier[:2] not in unique_identifiers:
+                        print(block_identifier)
+                        unique_identifiers[block_identifier[:2]] = [[], []]
+                    unique_identifiers[block_identifier[:2]][0].append(x)
+                    unique_identifiers[block_identifier[:2]][1].append(y)
+
+            for identifier, coords in unique_identifiers.items():
+                xs = coords[0]
+                ys = coords[1]
+                cmap_colors = [colorFader(colors[0], colors[1], i/len(xs[1:])) for i in range(len(xs))]
+
+                for i, (x, y) in enumerate(zip(xs[1:], ys[1:])):
+                    plt.plot(x, [yi-i*offset for yi in y], color=cmap_colors[i])
+
+                cmap = matplotlib.colors.ListedColormap(cmap_colors)
+                norm = mpl.colors.Normalize(0, config['sputter stop'])
+                cb1 = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), label='Sputter Time [min]')
+                cb1.ax.invert_yaxis()
+                plot_formatting(x, xunits, yunits, ylabel, legend=False)
+                plt.title(id_to_peak[identifier.strip()])
+                tikzplotlib.save(identifier+'_tikzplot.tex')
+                plt.savefig(identifier+'.svg')
+                plt.show()
+                #plt.show()
+    
+    if config['acsummry'] == "True":
+        x, ys, elmt_labels = read_acsummry(filepath+config['acname'])
+        linetypes = np.flip(['-', ':', '--', '-.'])
+        for i, (y, label) in enumerate(zip(np.flip(ys, 0), np.flip(elmt_labels))):
+            plt.plot(x, y, label=label, linestyle=linetypes[i], color=colorFader('black', 'gray', i/2))
+        plt.xlabel('Sputter Time [min]')
+        plt.ylim((0, 100))
+        plt.ylabel('Atomic Concentration [%]')
+        plt.legend()
+        plt.savefig('acsummary.svg')
+        plt.show()
 
 if __name__ == '__main__':
     main()
--- vamas_helpers.py
+++ vamas_helpers.py
@@ -1,32 +1,102 @@
 import matplotlib.pyplot as plt 
+import matplotlib as mpl
 import numpy as np
+from scipy.signal import find_peaks
 
 from VAMASparse import VAMASparser
 
-def plot_spectra(filepath, filenames, labels, colors, offset=0):
-    for i, (filename, label, color) in enumerate(zip(filenames, labels, colors)):
-        parser = VAMASparser(filepath+filename)
+# selected XPS binding energies (from ThermoFisher)
+xps_energies = {
+    'Sn':485.2, 'SnO':486, 'SnO2':486.6,
+    'In':443.8, 'In2O3':444, 'In2O3 3p3/2': 666.4,
+    'O (metal)': 529, 'C-O':533, 'C=O':532,
+    'Na': 1071,
+    'C-C': 284.8, 'C-O-C': 286, 'O-C=O': 288.5
+}
 
-        experiment, blocks = parser.read_VAMAS()
-
+def plot_spectra(parsers, labels, colors, offset=0, prominence=650, id=False):
+    for i, (parser, label, color) in enumerate(zip(parsers, labels, colors)):
         x, y, xunits, xlabel, yunits, ylabel  = get_binding_vs_y(parser)
 
         x = np.array(x)
         y = np.array(y)
         plt.plot(x, y+i*offset, label=label, color=color)
+        # prominence = 650 was basically the magic number for ITO spectra; may need to adjust for others
+        peaks, properties = find_peaks(y, prominence=prominence)
+
+        if id:
+            plt.plot(x[peaks], (y+i*offset)[peaks], 'x')
+            print(x[peaks])
+            for label, energy in xps_energies.items():
+                plt.axvline(x=energy)
+                plt.text(energy+0.1, 0, label)
+
     plot_formatting(x, xunits, yunits, ylabel)
     plt.show()
+    return x, y
 
-def get_binding_vs_y(parser):
-    x, xlabel, xunits = parser.get_x_vals()
+def get_binding_vs_y(parser, block_index=0):
+    x, xlabel, xunits = parser.get_x_vals(block_index)
     x_binding = [parser.kinetic_to_binding_energy(xi) for xi in x]
-    y, ylabel, yunits = parser.get_y_vals(0)
+    y, ylabel, yunits = parser.get_y_vals(0, block_index)
 
     return x_binding, y, xunits, xlabel, yunits, ylabel
 
-def plot_formatting(x, xunits, yunits, ylabel):
+def plot_formatting(x, xunits, yunits, ylabel, legend=True):
     plt.xlim(max(x)+1, min(x)-1)
     plt.xlabel('Binding Energy ['+xunits+']')
-    plt.ylabel(ylabel +' ['+yunits+']')
+    plt.ylabel(ylabel +' [a.u.]')
 
-    plt.legend()
+    plt.tick_params(
+        axis='y',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        left=False,      # ticks along the bottom edge are off
+        labelleft=False
+        )
+
+    if legend:
+        plt.legend()
+
+def colorFader(c1, c2, mix=0):
+    '''
+    credit to Markus Dutschke on Stack Overflow for this one
+    '''
+    c1 = np.array(mpl.colors.to_rgb(c1))
+    c2 = np.array(mpl.colors.to_rgb(c2))
+    return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
+
+def read_acsummry(filename):
+    '''
+    read the data out of an acsummry.txt file from the XPS
+    '''
+    is_data = False
+    counter = 0
+
+    x = []
+
+    with open(filename) as f:
+        read_data = f.read().splitlines()
+        for line in read_data:
+            words = line.split()
+            # skip header details
+            if not is_data and len(words) == 0:
+                pass
+            elif not is_data and not words[0] == 'Abscissa':
+                pass 
+            # get column labels
+            elif not is_data and words[0] == 'Abscissa':
+                is_data = True
+                labels = words[1:]
+                ys = [[] for i in range(len(labels))]
+            # skip RSF/CorrectedRSF lines
+            elif is_data and counter < 2:
+                counter += 1
+            # break to skip Mean/Standard Deviation lines
+            elif is_data and len(words) == 0:
+                break
+            # append percent concentration data
+            elif is_data: 
+                x.append(float(words[0]))
+                for i, word in enumerate(words[1:]):
+                    ys[i].append(float(word))
+    return x, ys, labels
